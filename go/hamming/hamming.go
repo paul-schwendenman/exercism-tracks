a8b3d78f17effa80735b5d79124eb1f14e30c152
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-07 17:21:52 -0500

    Add hamming solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,19 @@
 package hamming
 
+import "errors"
+
+// Distance returns the hamming distance of two strings
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("hamming: strings are of different length")
+	}
+
+	distance := 0
+
+	for pos := 0; pos < len(a); pos++ {
+		if a[pos] != b[pos] {
+			distance++
+		}
+	}
+	return distance, nil
 }
