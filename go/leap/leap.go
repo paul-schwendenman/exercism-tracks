ef7438686f549b80edbb4d8acf9bbade05b189c2
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-08 11:15:22 -0500

    Refactor leap solution

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,14 +2,5 @@ package leap
 
 // IsLeapYear returns true if the year would have an extra day
 func IsLeapYear(year int) bool {
-	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
-		return true
-	}
-	return false
+	return year%4 == 0 && (!(year%100 == 0) || (year%400 == 0))
 }
