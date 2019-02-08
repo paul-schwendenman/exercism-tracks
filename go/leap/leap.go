3c86b2c0caa4fb7e5eabe82be72438f5167fe880
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-08 11:18:44 -0500

    Refactor to extract common logic

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,5 +2,9 @@ package leap
 
 // IsLeapYear returns true if the year would have an extra day
 func IsLeapYear(year int) bool {
-	return year%4 == 0 && (!(year%100 == 0) || (year%400 == 0))
+	return isDivisbleBy(year, 4) && (!isDivisbleBy(year, 100) || isDivisbleBy(year, 400))
+}
+
+func isDivisbleBy(number int, divisor int) bool {
+	return number%divisor == 0
 }
