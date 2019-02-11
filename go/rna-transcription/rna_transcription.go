1cc023dcf513f231d36ab13cf35539db77f86e80
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-11 16:17:56 -0500

    Add rna conversion solution

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,18 @@
 package strand
 
+var dnaToRna = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA converts dna strand to rna
 func ToRNA(dna string) string {
+	rna := ""
+
+	for _, nucleotide := range dna {
+		rna += string(dnaToRna[nucleotide])
+	}
+	return rna
 }
