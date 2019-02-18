11861d59d2ffcf5511d3f80e22a2cf040a01a260
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-18 10:14:55 -0500

    Add bob solution for elixir

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,12 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      String.match?(input, ~r/^\s*$/) -> "Fine. Be that way!"
+      String.match?(input, ~r/^[\d ,]+$/) -> "Whatever."
+      String.match?(input, ~r/^[A-Z' ]+\?$/) -> "Calm down, I know what I'm doing!"
+      String.match?(input, ~r/^.+\?$/) -> "Sure."
+      String.match?(input, ~r/^[^a-z]+!?$/) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end
