4f65f73b0a3c0d8d59d953a0f0506747ec991256
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-19 11:04:22 -0500

    Refactor acronym solution

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,13 +1,6 @@
 import re
 
 
-def get_letter(word):
-    for char in word:
-        if char.isalpha():
-            break
-    return char.upper()
-
-
 def abbreviate(words):
-    word_list = re.split(r'[ -]+', words)
-    return ''.join(filter(lambda char: char.isalpha(), map(get_letter, word_list)))
+    word_list = re.findall(r'([A-Za-z])[A-Za-z\']*', words)
+    return ''.join(word_list).upper()
