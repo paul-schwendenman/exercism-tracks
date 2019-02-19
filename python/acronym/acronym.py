71f981ca08126e569f15d3b16cfeb6bb18770198
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-19 10:18:17 -0500

    Add acronym solution

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,13 @@
+import re
+
+
+def get_letter(word):
+    for char in word:
+        if char.isalpha():
+            break
+    return char.upper()
+
+
 def abbreviate(words):
-    pass
+    word_list = re.split(r'[ -]+', words)
+    return ''.join(filter(lambda char: char.isalpha(), map(get_letter, word_list)))
