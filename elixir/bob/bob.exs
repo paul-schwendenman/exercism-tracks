511d4d29abd2ef2ee65262b8a794e2da5f34e2f6
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-20 13:44:54 -0500

    Remove is_ prefix which is reserved for guard clauses

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,29 +1,29 @@
 defmodule Bob do
   def hey(input) do
-    is_yelling = is_yelling?(input)
-    is_question = is_question?(input)
+    yelling = yelling?(input)
+    question = question?(input)
 
     cond do
-      is_question && is_yelling -> "Calm down, I know what I'm doing!"
-      is_question -> "Sure."
-      is_yelling -> "Whoa, chill out!"
-      is_silence?(input) -> "Fine. Be that way!"
+      question && yelling -> "Calm down, I know what I'm doing!"
+      question -> "Sure."
+      yelling -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
       true -> "Whatever."
     end
   end
 
-  defp is_yelling?(normal) do
+  defp yelling?(normal) do
     upper = String.upcase(normal)
     lower = String.downcase(normal)
 
     (upper == normal) && (upper != lower)
   end
 
-  defp is_silence?(input) do
+  defp silence?(input) do
     String.trim(input) == ""
   end
 
-  defp is_question?(input) do
+  defp question?(input) do
     String.ends_with?(input, "?")
   end
 end
