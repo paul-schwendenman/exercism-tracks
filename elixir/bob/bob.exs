629b03cd0dd99a4bbcfa398129604e2dbac9e321
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-20 10:59:47 -0500

    Refactor based on feedback

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,12 +1,29 @@
 defmodule Bob do
   def hey(input) do
+    is_yelling = is_yelling?(input)
+    is_question = is_question?(input)
+
     cond do
-      String.match?(input, ~r/^\s*$/) -> "Fine. Be that way!"
-      String.match?(input, ~r/^[\d ,]+$/) -> "Whatever."
-      String.match?(input, ~r/^[A-Z' ]+\?$/) -> "Calm down, I know what I'm doing!"
-      String.match?(input, ~r/^.+\?$/) -> "Sure."
-      String.match?(input, ~r/^[^a-z]+!?$/) -> "Whoa, chill out!"
+      is_question && is_yelling -> "Calm down, I know what I'm doing!"
+      is_question -> "Sure."
+      is_yelling -> "Whoa, chill out!"
+      is_silence?(input) -> "Fine. Be that way!"
       true -> "Whatever."
     end
+  end
+
+  defp is_yelling?(normal) do
+    upper = String.upcase(normal)
+    lower = String.downcase(normal)
+
+    (upper == normal) && (upper != lower)
+  end
+
+  defp is_silence?(input) do
+    String.trim(input) == ""
+  end
+
+  defp is_question?(input) do
+    String.ends_with?(input, "?")
   end
 end
