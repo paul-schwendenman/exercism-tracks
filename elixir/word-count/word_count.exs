a74ed9179d890ee424d7894efb75ac1ce294f762
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-02-20 14:52:12 -0500

    Add logic to ignore underscores

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -8,7 +8,7 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> String.downcase
-    |> String.split(" ")
+    |> String.split(~r/[ _]/)
     |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
   end
 end
